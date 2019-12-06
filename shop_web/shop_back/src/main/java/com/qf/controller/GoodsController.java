bd86f55ae836106e6b1a75dced41f69809a2e12a
TY <12345@qq.com>
2019-12-06 21:06:50 +0800

    第6次 使用fast这傻逼玩意有bug，再不行直播把电脑砸了

--- shop_web/shop_back/src/main/java/com/qf/controller/GoodsController.java
+++ shop_web/shop_back/src/main/java/com/qf/controller/GoodsController.java
@@ -1,24 +1,19 @@
 package com.qf.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.qf.entity.Goods;
 import com.qf.entity.ResultData;
 import com.qf.service.IGoodsService;
-import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/goods")
@@ -27,7 +22,9 @@ public class GoodsController {
     @Reference
     private IGoodsService goodsService;
 
-    private String uploadPath = "G:/idea_workspace/imgs";
+    @Autowired
+    private FastFileStorageClient fastFileStorageClient;
+
 
     @RequestMapping("/list")
     public String list(Model model){
@@ -46,20 +43,39 @@ public class GoodsController {
     @RequestMapping("/uploader")
     @ResponseBody
     public ResultData<String> uploader(MultipartFile file){
-        String filename= UUID.randomUUID().toString();
-        String path=uploadPath+"/"+filename;
-        try (
-                InputStream inputStream = file.getInputStream();
-                OutputStream OutputStream = new FileOutputStream(path);
-             ){
-            IOUtils.copy(inputStream,OutputStream);
+//        String filename= UUID.randomUUID().toString();
+//        String path=uploadPath+"/"+filename;
+//        try (
+//                InputStream inputStream = file.getInputStream();
+//                OutputStream OutputStream = new FileOutputStream(path);
+//             ){
+//            IOUtils.copy(inputStream,OutputStream);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("file是"+file);
+        String path=null;
+        try {
+            StorePath storePath = fastFileStorageClient.uploadImageAndCrtThumbImage(
+                    file.getInputStream(),
+                    file.getSize(),
+                    "JPG",
+                    null
+
+            );
+            System.out.println("dsads21321321");
+            path=storePath.getFullPath();
+            System.out.println("path"+path);
         } catch (Exception e) {
             e.printStackTrace();
+
         }
-        return new ResultData<String>().setCode(ResultData.ResultCodeList.OK).setData(path);
+
+        System.out.println("这个傻逼玩意");
+        return new ResultData<String>().setCode(ResultData.ResultCodeList.OK).setData("http://www.img.com:8888"+path);
     }
 
-    @RequestMapping("/showimg")
+/*    @RequestMapping("/showimg")
     public void showimg(String imgPath, HttpServletResponse response){
         System.out.println("路径"+imgPath);
         try (
@@ -70,7 +86,9 @@ public class GoodsController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+
+
+    }*/
 
     @RequestMapping("insert")
     public String insert(Goods goods){
