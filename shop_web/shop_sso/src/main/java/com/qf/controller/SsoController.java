8ca48f97d9bd8ef785079a1b89187a6a6d233528
TY <657695528@qq.com>
2019-12-13 02:37:30 +0800

    第2次 登录与判断状态是否登录

--- shop_web/shop_sso/src/main/java/com/qf/controller/SsoController.java
+++ shop_web/shop_sso/src/main/java/com/qf/controller/SsoController.java
@@ -2,16 +2,28 @@ package com.qf.controller;
 
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.qf.entity.ResultData;
 import com.qf.entity.User;
 import com.qf.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/sso")
 public class SsoController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Reference
     private IUserService userService;
@@ -49,6 +61,44 @@ public class SsoController {
         return "register";
     }
 
+    /**
+     * 登录
+     */
 
+    @RequestMapping("login")
+    public String login(String username, String password, HttpServletResponse response){
+        System.out.println("登录的用户名"+username+"密码"+password);
+        User user = userService.queryByUserName(username);
+        System.out.println(user);
+        if(user != null && password.equals(user.getPassword())){
+            System.out.println("11111");
+            String token = UUID.randomUUID().toString();
+            redisTemplate.opsForValue().set(token , user);
+            Cookie loginToken = new Cookie("loginToken", token);
+            loginToken.setMaxAge(60 * 60 * 24 * 7);
+            response.addCookie(loginToken);
+            return "http://localhost";
+        }
+
+        return "redirect:/sso/toLogin";
+    }
+
+    /**
+     * 判断是否登录
+     * jsonp
+     */
+    @RequestMapping("isLogin")
+    @ResponseBody
+    public String isLogin(@CookieValue(value = "loginToken", required = false) String loginTokin, String callback){
+
+        ResultData<User> resultData = new ResultData<User>().setCode(ResultData.ResultCodeList.ERROR);
+        if(loginTokin != null){
+            User user = (User) redisTemplate.opsForValue().get("loginToken");
+            if (user != null){
+                resultData.setCode(ResultData.ResultCodeList.OK).setData(user);
+            }
+        }
+        return callback != null ? callback + "(" + JSON.toJSONString(resultData) + ")" : JSON.toJSONString(resultData);
+    }
 
 }
